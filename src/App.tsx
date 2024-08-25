fdd74c1cb5b5648d95abc3229721f97391eb6497
Steve Li <lithafnium2000@gmail.com>
2024-08-25 16:05:04 -0400

    router, upgraded vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-
 import Home from "@app/pages/home/home";
-import { Navbar } from "@app/shared/components/index";
-function App() {
-  return (
-    <>
-      <Navbar />
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "@app/shared/utils/routes";
 
-      <Home />
-    </>
-  );
+const router = createBrowserRouter([{ path: routes.HOME, element: <Home /> }]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
